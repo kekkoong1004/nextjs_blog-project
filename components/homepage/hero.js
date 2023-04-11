b0a155cd7ac79cea2ca0ff6b22c66feeb42203f1
kekkoong <kekkoong2062@gmail.com>
2023-04-11 14:44:38 +0800

    Initial commit

---
+++ components/homepage/hero.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import styles from './hero.module.css';
+
+function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src="/hero.jpg"
+          alt="This is a image of programmer"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Shawn</h1>
+      <p>
+        I am doing web development, specialized in React, NextJS, React Native
+        and NodeJS.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ pages/posts/index.js
@@ -0,0 +1,7 @@
+export default function AllPostsPage() {
+  return (
+    <>
+      <h1>All Posts Page</h1>
+    </>
+  );
+}
